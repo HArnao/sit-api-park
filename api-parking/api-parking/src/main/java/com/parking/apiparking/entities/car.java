e57194301440f89c1f6ff6728246d5eb740de246
HArnao <harnaol1@upao.edu.pe>
2023-04-16 18:14:25 -0500

    feat: Crea la entidad Car

--- api-parking/api-parking/src/main/java/com/parking/apiparking/entities/car.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/entities/car.java
@@ -1,5 +1,38 @@
 package com.parking.apiparking.entities;
 
-public class car {
+import java.time.LocalDateTime;
 
+public class car {
+    private String licensePlate;
+    private String color;
+    private LocalDateTime entryTime;
+
+    public car(String licensePlate, String color) {
+        this.licensePlate = licensePlate;
+        this.color = color;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(LocalDateTime entryTime) {
+        this.entryTime = entryTime;
+    }
 }
